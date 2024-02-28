7efee1645ddb58ac70391188f7f925f6f680c32e
Harun BIswas Rubel <harunbiswasrubel@gmail.com>
2024-02-28 18:23:27 +0600

    update base url

--- src/utility/Config.js
+++ src/utility/Config.js
@@ -1,7 +1,7 @@
-export const MONGODB_CONNECTION =
-  'mongodb+srv://testuser7777:testuser7777@cluster0.7uslu.mongodb.net/MernEcommerce?retryWrites=true&w=majority'
 // export const MONGODB_CONNECTION =
-//   'mongodb://127.0.0.1:27017/automanagement?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1'
+//   'mongodb+srv://testuser7777:testuser7777@cluster0.7uslu.mongodb.net/MernEcommerce?retryWrites=true&w=majority'
+export const MONGODB_CONNECTION =
+  'mongodb://127.0.0.1:27017/automanagement?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1'
 
 export const JWT_SECRET = 'sdjflkjsdjfjewrsdfjjhherojuwerfjower123-XYZ-ABC'
 export const JWT_EXPIRATION_TIME = '30d'
@@ -16,7 +16,7 @@ export const MAX_URL_ENCODED_SIZE = '50mb'
 
 export const email = 'admin@gmail.com'
 export const pass = 'admin1234'
-export const baseUrl = 'http://localhost:4000/'
+export const baseUrl = 'http://195.35.0.234:4000/'
 
 export const REQUEST_LIMIT_TIME = 15 * 60 * 1000 // 15 Min
 export const REQUEST_LIMIT_NUMBER = 3000 // Per 15 Min 3000 Request Allowed
