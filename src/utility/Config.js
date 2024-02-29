67f704c66823c1cebebd7771b079ceda1ddb4c75
Harun BIswas Rubel <harunbiswasrubel@gmail.com>
2024-02-29 11:55:18 +0600

    Update

--- src/utility/Config.js
+++ src/utility/Config.js
@@ -16,7 +16,7 @@ export const MAX_URL_ENCODED_SIZE = '50mb'
 
 export const email = 'admin@gmail.com'
 export const pass = 'admin1234'
-export const baseUrl = 'http://195.35.0.234/'
+export const baseUrl = 'http://backend.automanagement.co.uk/'
 
 export const REQUEST_LIMIT_TIME = 15 * 60 * 1000 // 15 Min
 export const REQUEST_LIMIT_NUMBER = 3000 // Per 15 Min 3000 Request Allowed
